09be56048f0a52ac5f3a500cbfdd2de46ff09d1e
Sam O'Keefe <samok13@gmail.com>
2016-01-14 18:02:40 -0800

    attempt at parse_tree

--- parse_tree.rb
+++ parse_tree.rb
@@ -10,7 +10,14 @@ class ParseTree
 
   def initialize(html_code)
   	regex = /<.*?>/
-    @tag_type = html_code.scan(regex)
+    @tag_type = html_string.scan(regex)
+    regex = />(.*?)</
+    @text_list = html_string.scan(regex)
+    @text_list.each do |text|
+      text.each do |text_item|
+      text_item.strip!
+      end
+    end
     # @parse = ParseTag.new(html_code)
     @root = nil
   end
@@ -19,15 +26,44 @@ class ParseTree
     @root = Tag.new(first_tag)
   end
 
+  def add_node(new_node)
+    new_node = Tag.new()
+  end
+
+  def add_child_node(new_node, parent)
+  end
+
   def make_tree
+    #if we don't have a root, make one
     unless @root
       add_root_node(@tag_type[0]) #first open tag
     end
+
+    current_node = @root
+    
+    #for each element of the tag_type array check if it has a /
+    #if element has no /, go to text_list and get child
+    #make a child node
+    #if element has a /, make node
+    @tag_type.each do |tag|
+      regex = /<\//
+      if !tag.match(regex)
+       add_node(tag)
+      elsif 
+        add_child_node()#create child node 
+        text_list_index += 1
+      end
 
     while @html_code.length > 0
       add_node(???????????)
     end
   end
+
+#tag_type
+#["<div>", "<p>", "</p>", "<div>", "</div>", "</div>"] 
+
+#text_list
+#[["div text before"], ["p text"], [""], ["more div text"], ["div text after"]] 
 
   # def add_node()
 		# if current node is nil
@@ -43,7 +79,7 @@ html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div te
 tree = ParseTree.new(html_string)
 tree.make_tree
 
-# regex = /<.*?>/
-# p html_string.scan(/<.*?>/)
-# p html_string.split(/<.*?>/)
+ regex = /<.*?>/
+p html_string.scan(/<.*?>/)
+p html_string.scan(/>(.*?)</)
 
